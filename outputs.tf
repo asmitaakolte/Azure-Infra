6228d872f478277d536d76a7eb8d3258d4e881d2
asmitaakolte <144066811+asmitaakolte@users.noreply.github.com>
2025-01-04 17:14:57 -0600

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.example.name
-  description = "The name of the created resource group."
-}
-
-output "resource_group_location" {
-  value       = azurerm_resource_group.example.location
-  description = "The location of the created resource group."
-}
